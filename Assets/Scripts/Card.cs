fd03984286d491830bd31dc757665774345ecbff
Jorge Eduardo <jorge.torres@inmersys.com>
2022-09-27 13:27:00 -0500

    moficiaciones a card manager

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public string nameCard;
+    public Image imageCarta,imageCartaBack;
+    private void Start()
     {
-        
+        //generateRandomCard();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void generateCard(string name, Sprite sprite)
     {
-        
+        nameCard = name;
+        imageCarta.sprite = sprite;
+        imageCartaBack.sprite = sprite;
     }
 }
--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -4,23 +4,44 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-    public List<ObjetcToCraft> totalObjects;
-    public List<string> cardsToFind;
-    public ObjetcToCraft currentObjectToCraft;
+    public List<Sprite> listCartas;
+    public List<Card> myLoteriaTable;
+    public List<Card> currentLoteriaTable;
+    public GameObject cardPrefab;
+    public static CardManager ManagerCardInstance;
     // Start is called before the first frame update
+
+
     void Start()
     {
-        
+        ManagerCardInstance = this;
     }
+    public void generateLoteriaTable()
+    {
+        int randomN = Random.Range(0,listCartas.Count);
+        string actualNameCard = listCartas[randomN].name;
+        //if (myLoteriaTable.Contains()){
 
+        //}
+    }
+    public void serchForMySprite(string nameSprite)
+    {
+        for (int i = 0; i < CardManager.ManagerCardInstance.listCartas.Count; i++)
+        {
+            if (CardManager.ManagerCardInstance.listCartas[i].name == nameSprite)
+            {
+                //imageCarta.sprite = CardManager.ManagerCardInstance.listCartas[i];
+                //imageCartaBack.sprite = CardManager.ManagerCardInstance.listCartas[i];
+            }
+            else
+            {
+                print("no se encontro ningun elemento con ese name");
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
-}
-[System.Serializable]
-public class ObjetcToCraft{
-    public string name;
-    public List<string> listComponents;
 }
